396ff69951efb676090e1f24da4fb8dc64b7551b
Noah_Orberg <noah.orberg.00@gmail.com>
2016-11-21 20:07:16 +0900

    update license infomation

--- rsa-report/src/Lib.hs
+++ rsa-report/src/Lib.hs
@@ -7,10 +7,8 @@ import Sec
 toString :: Integer -> String
 toString n = show n
 
-someFunc :: IO ()
-someFunc = do
-  putStrLn "Input number [\"p\", \"q\"] ->  "
-  pq <- fmap (read :: String -> Integer) . words <$> getLine
+continue :: [Integer] -> IO ()
+continue pq = do
   let n  = (pq !! 0)     * (pq !! 1)
       w  = (pq !! 0 - 1) * (pq !! 1 - 1)
   putStr "Please select e-number : "
@@ -18,7 +16,21 @@ someFunc = do
   es <- fmap (read :: String -> Integer) . words <$> getLine
   let e  = es !! 0
       d  = getD e w
-  if gcd e w /= 1 then do
-                  putStrLn "ERR : This is not e-number"
-                  return ()
-                  else putStrLn $ "Public_key is (" ++ (toString n) ++ ", " ++ (toString e) ++ "), Secret_key is " ++ (toString d)
+  if gcd e w /= 1
+     then do
+       putStrLn "ERR : This is not e-number"
+       return ()
+     else
+       putStrLn $ "Public_key is (" ++ (toString n) ++ ", " ++ (toString e) ++ "), Secret_key is " ++ (toString d)
+
+someFunc :: IO ()
+someFunc = do
+  putStrLn "Input p, q number ->  "
+  pq <- fmap (read :: String -> Integer) . words <$> getLine
+  if and [isPrime (pq !! 0), isPrime (pq !! 1)] 
+     then
+       continue pq
+     else do
+       putStrLn "ERR : p or q is not prime number"
+       return ()
+
